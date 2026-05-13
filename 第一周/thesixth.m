a6dbe5e77fccdc44bf3eb6fdefca34def39a94c8
reverse_1999 <2914824866@qq.com>
2026-05-13 11:03:15 +0800

    thefirst

--- 第一周/thesixth.m
+++ 第一周/thesixth.m
@@ -9,26 +9,26 @@ x1 = (n==3);
 x2 = (n>=0 & n<5);
 x3 = cos(2*pi/3*n)+sin(3*pi/10*n);
 %%%%%%%求x1零输入、零状态、全响应%%%%%%%%
-subplot(3,1,1);
+figure;
 y11 = filter(b,a,zeros(size(n)),z);
 stem(n,y11);
 xlabel('n');
 ylabel('y1(n)');
-title('零输入响应');
-subplot(3,1,2);
+title('x1零输入响应');
+figure;
 y12 = filter(b,a,x1,zeros(length(a)-1, 1));
 stem(n,y12);
 xlabel('n');
 ylabel('y2(n)');
-title('零状态响应');
+title('x1零状态响应');
 
-subplot(3,1,3);
+figure;
 %y13 = y11+y12;
 y13 = filter(b,a,x1,z);
 stem(n,y13);
 xlabel('n');
 ylabel('y13(n)');
-title('全响应');
+title('x1全响应');
 
 %%%%%%%求x2零输入、零状态、全响应%%%%%%%%
 figure;
@@ -36,14 +36,14 @@ y21 = filter(b,a,zeros(size(n)),z);
 stem(n,y21);
 xlabel('n');
 ylabel('y1(n)');
-title('零输入响应');
+title('x2零输入响应');
 
 figure;
 y22 = filter(b,a,x2,zeros(length(a)-1, 1));
 stem(n,y22);
 xlabel('n');
 ylabel('y2(n)');
-title('零状态响应');
+title('x2零状态响应');
 
 figure;
 %y23 = y21+y22;
@@ -51,7 +51,7 @@ y23 = filter(b,a,x2,z);
 stem(n,y23);
 xlabel('n');
 ylabel('y23(n)');
-title('全响应');
+title('x2全响应');
 
 %%%%%%%求x3零输入、零状态、全响应%%%%%%%%
 figure;
@@ -59,14 +59,14 @@ y31 = filter(b,a,zeros(size(n)),z);
 stem(n,y31);
 xlabel('n');
 ylabel('y1(n)');
-title('零输入响应');
+title('x3零输入响应');
 
 figure;
 y32 = filter(b,a,x3,zeros(length(a)-1, 1));
 stem(n,y32);
 xlabel('n');
 ylabel('y2(n)');
-title('零状态响应');
+title('x3零状态响应');
 
 figure;
 %y33 = y31+y32;
@@ -74,4 +74,4 @@ y33 = filter(b,a,x3,z);
 stem(n,y33);
 xlabel('n');
 ylabel('y33(n)');
-title('全响应');
+title('x3全响应');
--- 第一周/thethird.m
+++ 第一周/thethird.m
@@ -1,6 +1,12 @@
 %%%(1)%%%
 b1 = [1 0 -0.5 0 0];
 a1 = [1 -1 1 0 0.25];
+poles1 = roots(a1);
+if all(abs(poles1) < 1)
+	disp('系统一稳定');
+else
+	disp('系统一不稳定');
+end
 figure;
 subplot(2,1,1);
 hn = impz(b1,a1,32);
@@ -26,6 +32,12 @@ title('filtic和filter函数求解的阶跃响应');
 %%%(2)%%%
 b2 = [1 0.5 -0.5 -1 -0.5 1];
 a2 = [1 0 0 0 0 0];
+poles2 = roots(a2);
+if all(abs(poles2) < 1)
+	disp('系统二稳定');
+else
+	disp('系统二不稳定');
+end
 figure;
 subplot(2,1,1);
 hn = impz(b2,a2,32);
