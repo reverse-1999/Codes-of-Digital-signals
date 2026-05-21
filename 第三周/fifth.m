ab7ebce4da1fafdbb1c41c5c14be2c7fa5a2b487
reverse <2914824866@qq.com>
2026-05-21 11:50:13 +0800

    halfthird

--- 第三周/fifth.m
+++ 第三周/fifth.m
@@ -16,3 +16,26 @@ title('fs=10Hz时的sinc函数');
 xlabel('t');
 ylabel('sinc(t)');
 
+t_des = linspace(-2,2,201);
+N0 =201;
+T = 1/fs2;
+Y = zeros(size(t_des));
+N = 9;
+for i = 1:N0
+    result = 0;
+    tp = t_des(i);
+    nq = round(tp/T);
+    if  == 1
+        disp(i);
+    end
+    for k = -4:4
+    idx = nq+k;
+    if idx<1 || idx>41
+        continue;
+    end
+    result = result + f2t(idx)*sinc(idx*T);
+    end
+    Y(i) = result;
+end
+figure;
+stem(t_des,Y);
